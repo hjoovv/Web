b4421e44a27e8a341f592aab0bec536899dee6eb
hjoo <114377552+hjoovv@users.noreply.github.com>
2025-02-11 13:04:06 +0900

    Update macro.js

--- macro.js
+++ macro.js
@@ -1,8 +1,8 @@
 var waittime = 500;
 var loop = 2;
 var count = 0;
-var paramarr = ['GESO101', 'GEST045', 'IFLS012', 'GELI002'];
-var numarr = ["00", "00", "D9", "7D"];
+var paramarr = ['COSE222', 'COSE221', 'COSE389', 'COSE455'];
+var numarr = ["00", "01", "00", "00"];
 NetFunnel.TS_BYPASS = true;
 NetFunnel.TS_MAX_NWAIT_COUNT = 0;
 
